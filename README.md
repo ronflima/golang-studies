b0eef872bc143eb2b651d736ac9d2fc27fc847e2
Ronaldo Faria Lima <ronaldo@Alienaldo>
2024-07-29 09:56:33 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Go Studies
+
+This repo contains my studies about Go language. Every single project and test
+is placed in a different directory inside this repository. 
---
+++ src/farenheit_to_celsius.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Ronaldo Faria Lima
+ * ===
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g ºF = %g ºC\n", freezingF, fToC(freezingF))
+	fmt.Printf("%g ºF = %g ºC\n", boilingF, fToC(boilingF))
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
